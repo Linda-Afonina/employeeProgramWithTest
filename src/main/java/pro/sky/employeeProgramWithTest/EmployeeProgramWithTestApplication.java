9d73722f285cdb9395ed57aa02b4168d6476f4f9
linda <charming.linda85@gmail.com>
2024-07-04 16:28:00 +0300

    Initial commit

---
+++ src/main/java/pro/sky/employeeProgramWithTest/EmployeeProgramWithTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeProgramWithTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProgramWithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProgramWithTestApplication.class, args);
+	}
+
+}
